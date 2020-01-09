3138d217bcc04844989c5e46798abddcda4fca62
HagiaSofiya <sofiyashrayber@gmail.com>
2020-01-09 14:40:16 -0500

    More button examples

--- example/src/App.js
+++ example/src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react'
 
-import {ColoredHeadingOne , Button } from 'pb-test'
+import { Button, CodeButton , LinkButton , FaucetRequestButton} from 'pb-test'
 
 
 export default class App extends Component {
 	render () {
 		return (
 			<div>
-				<ColoredHeadingOne
-					text="Colored Heading"
-					color={"red"}
-				/>
-				<Button
-					text="I am Button"
-				/>
+				<Button text="Button"/>
+				<CodeButton text="Code Button"/>
+				<LinkButton text="Link Button" />
+				<FaucetRequestButton text="Faucet Request Button" />
 			</div>
 		)
 	}
--- src/index.js
+++ src/index.js
@@ -4,22 +4,6 @@ import PropTypes from 'prop-types'
 import styles from './styles.css'
 
 
-export class ColoredHeadingOne extends Component {
-	static propTypes = {
-		text: PropTypes.string,
-		color: PropTypes.string
-	}
-
-	render() {
-		const { text, color } = this.props
-
-		return (
-			<h1 style={{ color }}>{text}</h1>
-		)
-	}
-}
-
-
 
 export class Button extends Component {
 	static propTypes = {
@@ -31,6 +15,49 @@ export class Button extends Component {
 
 		return (
 			<button>{text}</button>
+		)
+	}
+}
+
+
+export class CodeButton extends Component {
+	static propTypes = {
+		text: PropTypes.string,
+	}
+
+	render() {
+		const { text } = this.props
+
+		return (
+			<button className={styles.codeCopy}>{text}</button>
+		)
+	}
+}
+
+export class LinkButton extends Component {
+	static propTypes = {
+		text: PropTypes.string,
+	}
+
+	render() {
+		const { text } = this.props
+
+		return (
+			<button className={styles.link}>{text}</button>
+		)
+	}
+}
+
+export class FaucetRequestButton extends Component {
+	static propTypes = {
+		text: PropTypes.string,
+	}
+
+	render() {
+		const { text } = this.props
+
+		return (
+			<button className={styles.faucetRequest}>{text}</button>
 		)
 	}
 }
